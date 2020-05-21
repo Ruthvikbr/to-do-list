0de281d26c7654e4bc90ba9ac2cf11bbdbe37e52
Ruthvikbr <ruthvikbr24@gmail.com>
2020-05-21 19:59:50 +0530

    add TaskDatabase

--- FinalApp/app/src/main/java/com/workshop/finalapp/Database/TaskDatabase.java
+++ FinalApp/app/src/main/java/com/workshop/finalapp/Database/TaskDatabase.java
@@ -1,4 +1,50 @@
 package com.workshop.finalapp.Database;
 
-public class TaskDatabase {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.workshop.finalapp.data.Task;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Task.class},version = 1,exportSchema = false)
+public abstract class TaskDatabase extends RoomDatabase {
+
+    public abstract TaskDao taskDao();
+
+    public static TaskDatabase INSTANCE = null;
+
+    private static ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public static  TaskDatabase getInstance(final Context context){
+        if(INSTANCE==null){
+            synchronized (TaskDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskDatabase.class,
+                            "TaskDatabase"
+                            ).addCallback(new Callback() {
+                        @Override
+                        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                            super.onCreate(db);
+                            executor.execute(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                }
+                            });
+                        }
+                    }).fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
 }
